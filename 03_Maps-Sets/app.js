d17f35d9bedffa35db1b16b6eea15223e39ecf64
widenhrzns <widenhrzns@gmail.com>
2025-01-06 18:22:10 +0300

    WeakSet

--- 03_Maps-Sets/app.js
+++ 03_Maps-Sets/app.js
@@ -1,31 +1,11 @@
 "use string";
 
-const map = new WeakMap();
-
 let a = { a: 1 };
-let b = { b: 1 };
-
-map.set(a, "testA").set(b, "testB");
-
-console.log(map.get(a));
-console.log(map.has(a));
-console.log(map);
+let b = { b: 2 };
+const set = new WeakSet([a, b]);
 
 a = null;
-console.log(map);
 
-//////////////////////////////
-let cache = new WeakMap();
-
-function getValue(obj) {
-  if (!cache.has(obj)) {
-    const res = 1;
-    cache.set(obj, res);
-  }
-  return cache.get(obj);
-}
-
-const res = getValue(b);
-console.log(res);
-const res2 = getValue(b);
-console.log(res2);
+setTimeout(() => {
+  console.log(set);
+}, 1000);
