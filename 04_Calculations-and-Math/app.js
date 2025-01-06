e1acd35a75c5b20208c6b055a5fae0ff412a7356
widenhrzns <widenhrzns@gmail.com>
2025-01-06 23:45:11 +0300

    Библиотека Math

--- 04_Calculations-and-Math/app.js
+++ 04_Calculations-and-Math/app.js
@@ -1,26 +1,21 @@
 "use strict";
 
-console.log(10 === 10.0);
-// 0 1
-console.log(0.1 + 0.2 === 0.3);
-console.log(0.1 + 0.2);
+console.log(Math.sqrt(36));
+console.log(Math.cbrt(27));
+console.log(16 ** (1 / 4));
 
-console.log(Number("10"));
-console.log(+"20");
+console.log(Math.sign(-100));
+console.log(Math.sign(100));
+console.log(Math.abs(-100));
 
-console.log(Number.parseInt("11", 10)); // второй параметр - система исчесления
-console.log(Number.parseInt("11 sec", 10));
-console.log(Number.parseInt("sec 11", 10));
+console.log(Math.exp(2));
 
-console.log(Number.parseFloat("11.5", 10));
-console.log(Number.parseFloat("11.5 sec", 10));
-console.log(Number.parseFloat("sec 11.5", 10));
+console.log(Math.max(1.5, -2, 10, 0, 19));
+console.log(Math.max(1.5, -2, "10", true, "19"));
+console.log(Math.max(1.5, -2, "10", true, "19s"));
+console.log(Math.min(1.5, -2, "10", true, "19"));
 
-console.log(Number.isNaN(Number("10fkjef")));
-console.log(Number.isNaN(10 / 0));
-console.log(10 / 0);
-console.log(Number.isFinite(10 / 0));
-console.log(Number.isFinite(Number("10fkjef")));
+const arr = [1.5, -2, 10, 0, 19];
+console.log(Math.max(...arr));
 
-console.log(Number.isInteger(10));
-console.log(Number.isInteger(10.5));
+console.log(Math.random());
