d7b7df1020da1abcbc8912138192284937d5002c
widenhrzns <widenhrzns@gmail.com>
2025-01-06 03:51:49 +0300

    Основы работы с Map

--- 03_Maps-Sets/app.js
+++ 03_Maps-Sets/app.js
@@ -1,28 +1,33 @@
 "use string";
 
-const flights = ["Russia", "USA", "London", "London", "USA"];
-console.log(flights);
+const weatherMap = new Map();
+weatherMap.set("London", "10").set("Moscow", "7"); // наполнение map (можно чейнить тк возвращает map)
 
-const setFlights = new Set(flights);
-console.log(setFlights);
-console.log(setFlights.size);
-console.log(setFlights.has("Russia"));
-// console.log(setFlights.has("s"));
+console.log(weatherMap);
 
-setFlights.add("Paris");
-setFlights.delete("London");
-console.log(setFlights);
+console.log(weatherMap.get("Moscow"));
+console.log(weatherMap.get("not found"));
 
-// setFlights.clear();
-// console.log(setFlights);
+console.log(weatherMap.has("Moscow"));
+console.log(weatherMap.has("not found"));
 
-for (const flight of setFlights) {
-  console.log(flight);
-}
+weatherMap.delete("London"); // нельзя чейнить тк возвращает не map а true/false
+console.log(weatherMap);
 
-console.log([...setFlights]);
+// weatherMap.clear();
+// console.log(weatherMap);
 
-const setObj = new Set([{ a: 1 }, { b: 2 }, { b: 2 }]);
-console.log(setObj);
+const array = [1, 2, 3];
 
-console.log(new Set("abcd"));
+weatherMap
+  .set(1, 5)
+  .set(true, "yes")
+  .set(false, "no")
+  .set(array, "array")
+  .set({ a: 1 }, { b: 1 });
+console.log(weatherMap);
+
+console.log(weatherMap.size);
+
+console.log(weatherMap.get(array));
+console.log(weatherMap.get({ a: 1 }));
