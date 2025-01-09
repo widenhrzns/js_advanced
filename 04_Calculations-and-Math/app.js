f3bcd08b01f8c7d3a0ea485794f9502d76a9c0b6
widenhrzns <widenhrzns@gmail.com>
2025-01-09 20:54:21 +0300

    Интернационализация чисел

--- 04_Calculations-and-Math/app.js
+++ 04_Calculations-and-Math/app.js
@@ -1,26 +1,33 @@
 "use strict";
 
-const max = 2 ** 53 - 1;
+const options1 = {
+  style: "currency",
+  currency: "RUB",
+  //   useGrouping: false,
+};
 
-console.log(Number.MAX_SAFE_INTEGER); // max доступное число
-console.log(max); // дальнейшие операции выше этого числа - небезопасные, тк мы молучем некорректный результат
-console.log(max + 1);
-console.log(max + 2);
-console.log(max + 3);
+const options2 = {
+  style: "currency",
+  currency: "USD",
+};
+console.log(new Intl.NumberFormat("ru-RU", options1).format(23000));
+console.log(new Intl.NumberFormat("en-US", options2).format(23000));
+// console.log(new Intl.NumberFormat("ar-SY", options2).format(23000));
 
-console.log(9481098190581958119248129398129319n); // BigInt
-console.log(BigInt(9481098190581958119248129398129319)); // лучше передавать строку, тк движок все равно будет работать с числом и будут возникать некорретные реузьтаты
-console.log(BigInt("9481098190581958119248129398129319"));
+const options3 = {
+  style: "decimal",
+};
+console.log(new Intl.NumberFormat("ru-RU", options3).format(10000));
 
-console.log(10n + 10n);
-// console.log(10n + 10); нельзя миксовать BigInt и другие типы без явной конвертации
-console.log(10n + BigInt(10));
+const options4 = {
+  style: "percent",
+};
+console.log(new Intl.NumberFormat("ru-RU", options4).format(0.8));
 
-console.log(10n * 10n);
-console.log(10n * BigInt(10));
+const options5 = {
+  style: "unit",
+  unit: "celsius",
+};
+console.log(new Intl.NumberFormat("ru-RU", options5).format(15));
 
-console.log(10n == 10); // для операторов сравнение приводить не нужно
-
-console.log(typeof 10n);
-
-console.log(10n / 3n); // остаток отбрасывается
+// https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Intl/NumberFormat
