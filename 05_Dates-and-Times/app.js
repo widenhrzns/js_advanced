34884aa02452088de4beb40689399e3e72fd1814
widenhrzns <widenhrzns@gmail.com>
2025-01-12 16:55:47 +0300

    Сравнение дат

--- 05_Dates-and-Times/app.js
+++ 05_Dates-and-Times/app.js
@@ -1,12 +1,15 @@
 "use strict";
 
-const date1 = new Date(2024, 10, 15);
-const date2 = new Date(2024, 11, 15);
-console.log(Number(date1));
-console.log(Number(date2));
-console.log(date2 - date1);
+// const first = new Date(2024, 10, 4);
+// const second = new Date(2024, 10, 3);
 
-function getDaysBetweenDates(dateFirst, dateSecond) {
-  return (dateSecond - dateFirst) / (1000 * 60 * 60 * 24);
-}
-console.log(getDaysBetweenDates(date1, date2));
+// console.log(first > second);
+// console.log(first.getTime() > second.getTime());
+
+const first = new Date(2024, 10, 4);
+const second = new Date(2024, 10, 4);
+
+console.log(first == second); // неравны тк сравниваются не значения а ссылки на объекты
+console.log(first === second);
+console.log(first.getTime() === second.getTime());
+console.log(Number(first) === Number(second));
