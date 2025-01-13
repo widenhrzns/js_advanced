f47b445ed2410480c9beec180ef3bdedfa8f0444
widenhrzns <widenhrzns@gmail.com>
2025-01-13 23:24:06 +0300

    Упражнение - Таймер пиццы

--- 06_Timers/app.js
+++ 06_Timers/app.js
@@ -1,9 +1,33 @@
 "use strict";
 
-const interval = setInterval(() => {
-  console.log(new Date());
-}, 1000);
+/*
+    Таймер пиццы 
+    Сделать таймер пиццы (функция, принимающая время),
+    который будет выводить в консоль секунды, 
+    оставшиеся до готовности пицы и сообщение по готовности. 
+    00:04 
+    00:03 
+    00:02 
+    00:01 
+    00:00 
+    🍕!!!
+*/
 
-setTimeout(() => {
-  clearInterval(interval);
-}, 5000);
+function pizzaTimer(ms = 5000) {
+  const end = new Date().getTime() + ms;
+  const interval = setInterval(() => {
+    console.log(
+      new Intl.DateTimeFormat(navigator.language, {
+        minute: "numeric",
+        second: "numeric",
+      }).format(end + 100 - new Date())
+    );
+    ms -= 1000;
+  }, 1000);
+  setTimeout(() => {
+    clearInterval(interval);
+    console.log("🍕!!!");
+  }, ms);
+}
+
+pizzaTimer(10000);
