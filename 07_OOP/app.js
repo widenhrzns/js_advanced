4b848e3cff7eb15ae66b35b4ca2e0b3c81975f9b
widenhrzns <widenhrzns@gmail.com>
2025-01-24 00:17:55 +0300

    Оператор new

--- 07_OOP/app.js
+++ 07_OOP/app.js
@@ -1,4 +1,19 @@
 "use strict";
 
-const a = [1];
-console.dir(a);
+const User = function (email, password) {
+  this.email = email;
+  this.password = password + "%%%";
+};
+
+const user1 = new User("a@a.ru", "123");
+console.log(user1);
+const user2 = new User("abc@a.ru", "321");
+console.log(user2);
+
+// Создается пустой объект
+// Вызывается User функция
+// this = пустому объекту
+// Объект связывается с prototype
+// Возвращается объект
+
+console.log(user2 instanceof User); // user2 является инстансом User
