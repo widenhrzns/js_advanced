4a9be1effb6a25b5b9fcf6a93f6f23766dadcd86
widenhrzns <widenhrzns@gmail.com>
2025-01-26 17:15:51 +0300

    Упражнение - класс пользователя

--- 08_Classes/app.js
+++ 08_Classes/app.js
@@ -1,29 +1,61 @@
 "use strict";
 
-class Car {
-  #vin = 6;
-  speed;
+/* 
+    Реализовать класс пользователя, со свойствами:
+    -логин
+    -пароль
 
-  constructor() {
-    // delete this.#vin // не сможем
-    this.#test2 = 5;
+    Причем пароль при установке должен переворачиваться и в таком виде храниться.
+
+    Пароль и логин после создания изменять нельзя.
+
+    Также у класса добавить методы:
+    -смены пароля(передаем старый и новый)
+    -сверки пароля
+ */
+
+class User {
+  #login;
+  #_password;
+
+  constructor(login, password) {
+    this.#login = login;
+    this.#password = password;
   }
 
-  #changeVin() {
-    console.log("changed");
+  set #password(password) {
+    this.#_password = password.split("").reverse().join("");
   }
 
-  test() {
-    // проверка
-    this.#changeVin();
+  get #password() {
+    return this.#_password.split("").reverse().join("");
   }
 
-  static #field = 3;
+  get login() {
+    return this.#login;
+  }
 
-  static {
-    this.#field = 5;
+  checkPassword(password) {
+    return this.#password === password;
+  }
+
+  changePassword(oldPassword, newPassword) {
+    if (!this.checkPassword(oldPassword)) {
+      return false;
+    }
+    this.#password = newPassword;
+    return true;
   }
 }
 
-const car = new Car();
-car.test();
+const user1 = new User("popa", "123456");
+console.log(user1);
+
+console.log(user1.login);
+
+console.log(user1.checkPassword("123"));
+console.log(user1.checkPassword("123456"));
+
+console.log(user1.changePassword("12345", "123"));
+console.log(user1.changePassword("123456", "123"));
+console.log(user1);
