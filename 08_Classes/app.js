f93e8e4855d3c2c409c9d3ed2680116caa6ea117
widenhrzns <widenhrzns@gmail.com>
2025-01-26 15:20:13 +0300

    Static

--- 08_Classes/app.js
+++ 08_Classes/app.js
@@ -1,45 +1,27 @@
 "use strict";
 
-const task = {
-  title: "Task1",
-  dueTo: new Date("2025/01/01"),
-
-  get isOverdue() {
-    return this.dueTo <= new Date();
-  },
-
-  set isOverdue(isOverdueTask) {
-    if (isOverdueTask) {
-      this.dueTo = new Date();
-    }
-  },
-};
-
-console.log(task.isOverdue);
-
-task.isOverdue = true;
-console.log(task);
-
-///
-
-class Task {
-  constructor(title, dueDate) {
-    this.title = title;
-    this.dueDate = dueDate;
+class Test {
+  static a = 1;
+  static hello() {
+    console.log("Hello");
   }
 
-  get isOverdue() {
-    return this.dueDate <= new Date();
-  }
-
-  set dueDate(date) {
-    if (date < new Date()) {
-      return;
-    }
-    this._dueDate = date;
+  static {
+    let b = 5;
+    this.a = 5;
   }
 }
 
-const newTask = new Task("Task2", new Date());
-console.log((newTask.dueDate = new Date("2026/01/01")));
-console.log(newTask);
+// const test = new Test();
+// test.hello();
+
+Test.hello();
+console.log(Test.a);
+
+///
+const Test2 = function () {};
+Test2.hello = function () {
+  console.log("Hello");
+};
+
+Test2.hello();
