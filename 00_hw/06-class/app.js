52fab93d9652c2eb5b7bcf017a6381a0d22e81e7
widenhrzns <widenhrzns@gmail.com>
2025-01-28 01:27:22 +0300

    6-class

--- 00_hw/06-class/app.js
+++ 00_hw/06-class/app.js
@@ -3,23 +3,15 @@
 class Car {
   #brand;
   #model;
-  #_mileage;
+  #mileage;
 
   constructor(brand, model, mileage) {
     this.#brand = brand;
     this.#model = model;
-    this.#mileage = mileage;
+    this.mileage = mileage;
   }
 
-  set #mileage(mileage) {
-    this.#_mileage = mileage;
-  }
-
-  get #mileage() {
-    return this.#_mileage;
-  }
-
-  changeMileage(newMileage) {
+  set mileage(newMileage) {
     if (this.#mileage === newMileage) {
       console.log(`mileage is equal to the original - no changes made`);
       return false;
@@ -32,23 +24,27 @@ class Car {
     return true;
   }
 
+  get mileage() {
+    return this.#mileage;
+  }
+
   info() {
     console.log(
       `Brand: ${this.#brand} \nModel: ${this.#model} \nMileage: ${
-        this.#_mileage
+        this.#mileage
       }`
     );
   }
 }
 
 const car1 = new Car("bwm", "m3", 13);
-car1.changeMileage(13);
-car1.changeMileage(10);
-car1.changeMileage(15);
+car1.mileage = 13;
+car1.mileage = 10;
+car1.mileage = 15;
 car1.info();
 console.log(car1);
 
 const car2 = new Car("audi", "q3");
 car2.info();
-car2.changeMileage(10);
+car2.mileage = 10;
 car2.info();
