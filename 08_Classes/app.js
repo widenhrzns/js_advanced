906ffea604b9bcf91fb54a146831359d4b6758b9
widenhrzns <widenhrzns@gmail.com>
2025-01-27 12:31:22 +0300

    Object.create

--- 08_Classes/app.js
+++ 08_Classes/app.js
@@ -1,61 +1,25 @@
 "use strict";
 
-/* 
-    Реализовать класс пользователя, со свойствами:
-    -логин
-    -пароль
+const User = {
+  init(email, password) {
+    this.email = email;
+    this.password = password;
+  },
+  log() {
+    console.log("log");
+  },
+};
 
-    Причем пароль при установке должен переворачиваться и в таком виде храниться.
-
-    Пароль и логин после создания изменять нельзя.
-
-    Также у класса добавить методы:
-    -смены пароля(передаем старый и новый)
-    -сверки пароля
- */
-
-class User {
-  #login;
-  #_password;
-
-  constructor(login, password) {
-    this.#login = login;
-    this.#password = password;
-  }
-
-  set #password(password) {
-    this.#_password = password.split("").reverse().join("");
-  }
-
-  get #password() {
-    return this.#_password.split("").reverse().join("");
-  }
-
-  get login() {
-    return this.#login;
-  }
-
-  checkPassword(password) {
-    return this.#password === password;
-  }
-
-  changePassword(oldPassword, newPassword) {
-    if (!this.checkPassword(oldPassword)) {
-      return false;
-    }
-    this.#password = newPassword;
-    return true;
-  }
-}
-
-const user1 = new User("popa", "123456");
+const user1 = Object.create(User);
 console.log(user1);
-
-console.log(user1.login);
-
-console.log(user1.checkPassword("123"));
-console.log(user1.checkPassword("123456"));
-
-console.log(user1.changePassword("12345", "123"));
-console.log(user1.changePassword("123456", "123"));
+user1.log();
+// user1.email = "a@2.ru";
+// user1.password = "123";
+user1.init("a@a.ru", "123");
 console.log(user1);
+console.log(user1.__proto__ === User);
+
+const admin = Object.create(user1);
+console.log(user1);
+admin.log();
+console.log(admin.email);
