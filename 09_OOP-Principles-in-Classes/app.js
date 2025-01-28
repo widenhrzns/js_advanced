baf530bbc8c0b64c011169819bd35f8b9a26ca7a
widenhrzns <widenhrzns@gmail.com>
2025-01-28 15:16:28 +0300

    Наследование в ES6

--- 09_OOP-Principles-in-Classes/app.js
+++ 09_OOP-Principles-in-Classes/app.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const Book = function (title, author) {
+/* const Book = function (title, author) {
   this.title = title;
   this.author = author;
 };
@@ -26,4 +26,31 @@ book.buy();
 console.log(AudioBook.prototype.constructor);
 
 console.log(book instanceof AudioBook);
-console.log(book instanceof Book);
+console.log(book instanceof Book); */
+
+class Book {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
+
+  buy() {
+    console.log("buy");
+  }
+}
+
+class AudioBook extends Book {
+  constructor(title, author, lenMin) {
+    super(title, author); //метод вызывающий конструктор исходного класса
+    this.lenMin = lenMin;
+  }
+
+  log() {
+    console.log(`${this.title} - ${this.lenMin}`);
+  }
+}
+
+const book = new AudioBook("ewkfow", "Prrr", 120);
+console.log(book);
+book.log();
+book.buy();
