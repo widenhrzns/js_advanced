da1078c59c82fca4c5815c0e2af549e14edb5706
widenhrzns <widenhrzns@gmail.com>
2025-01-30 16:34:45 +0300

    Принцип открытости и закрытости

--- 10_SOLID/app.js
+++ 10_SOLID/app.js
@@ -1,24 +1,29 @@
 "use strict";
 
-class Character {
-  #inventory = [];
-  #health = 10;
-
-  pickItem(item) {
-    this.#inventory.push(item);
-  }
-
-  receiveDamage(damage) {
-    this.#health -= damage;
-  }
+class Treasure {
+  value = 0;
 }
 
-class DB {
-  save(item) {
-    localStorage.satItem("char", item);
+class Coin extends Treasure {
+  value = 1;
+}
+
+class Crystal extends Treasure {
+  value = 10;
+}
+
+class Brilliant extends Treasure {
+  value = 20;
+}
+
+class Inventory {
+  #score;
+
+  pick(treasure) {
+    this.#score += treasure.value;
   }
 
-  loaCharacter() {
-    // ...
-  }
+  pickCoin() {}
+
+  pickCrystal() {}
 }
