2a9198e6eaa50a5596e6fe86f78abbf97646e6c0
widenhrzns <widenhrzns@gmail.com>
2025-01-30 17:02:00 +0300

    Принцип инверсии зависимостей

--- 10_SOLID/app.js
+++ 10_SOLID/app.js
@@ -1,19 +1,32 @@
 "use strict";
 
-class Weapon {
-  cost;
-
-  dealDamage() {}
-}
-
-class Rifle extends Weapon {
-  shoot() {
-    this.dealDamage();
+class DB {
+  save(items) {
+    console.log(`Saved: ${items}`);
   }
 }
 
-class Sword extends Weapon {
-  strike() {
-    this.dealDamage();
+class MongoDB extends DB {
+  save(items) {
+    console.log(`Saved to Mongo: ${items}`);
   }
 }
+
+class ToDoList {
+  items = [1, 2, 3];
+  db;
+
+  constructor(db) {
+    this.db = db;
+  }
+
+  saveToDB() {
+    this.db.save(this.items);
+  }
+}
+
+const list1 = new ToDoList(new DB());
+list1.saveToDB();
+
+const list2 = new ToDoList(new MongoDB());
+list1.saveToDB();
