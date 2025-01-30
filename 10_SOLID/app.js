6f4993be3442b5022ee2f0449f152753c4f8e307
widenhrzns <widenhrzns@gmail.com>
2025-01-30 16:50:35 +0300

    Принцип разделения интерфейса

--- 10_SOLID/app.js
+++ 10_SOLID/app.js
@@ -1,24 +1,19 @@
 "use strict";
 
-class User {
-  #role = "user";
+class Weapon {
+  cost;
 
-  getRole() {
-    return this.#role;
+  dealDamage() {}
+}
+
+class Rifle extends Weapon {
+  shoot() {
+    this.dealDamage();
   }
 }
 
-class Admin extends User {
-  #role = ["user", "admin"];
-
-  getRole() {
-    return this.#role.join(", ");
+class Sword extends Weapon {
+  strike() {
+    this.dealDamage();
   }
 }
-
-function logRole(user) {
-  console.log("Role: " + user.getRole().toUpperCase());
-}
-
-logRole(new User());
-logRole(new Admin());
