d1366c077ce59c3d608fa8052dc91fc226de28a0
widenhrzns <widenhrzns@gmail.com>
2025-02-01 23:24:41 +0300

    Упражнение - Расчёт цены

--- 11_Asynchronous-JS/app.js
+++ 11_Asynchronous-JS/app.js
@@ -1,18 +1,16 @@
 "use strict";
 
-function req(id) {
-  const request = new XMLHttpRequest();
-  request.open("GET", "https://dummyjson.com/products/" + id);
-  request.send();
+/*
+  Получить среднюю цену товара из API
+  https://dummyjson.com/products
+*/
 
-  request.addEventListener("load", function () {
-    const data = JSON.parse(this.responseText);
-    console.log(data);
-  });
-}
+const request = new XMLHttpRequest();
+request.open("GET", "https://dummyjson.com/products");
+request.send();
 
-req(1);
-req("");
-req(3);
-
-console.log("end");
+request.addEventListener("load", function () {
+  const { products } = JSON.parse(this.response);
+  const sum = products.reduce((acc, product) => (acc += product.price), 0);
+  console.log(sum / products.length);
+});
