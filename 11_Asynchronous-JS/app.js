205e965990bdabbe30d74397014f97222086ee23
widenhrzns <widenhrzns@gmail.com>
2025-02-01 17:39:43 +0300

    Получение данных

---
+++ 11_Asynchronous-JS/app.js
@@ -0,0 +1,18 @@
+"use strict";
+
+function req(id) {
+  const request = new XMLHttpRequest();
+  request.open("GET", "https://dummyjson.com/products/" + id);
+  request.send();
+
+  request.addEventListener("load", function () {
+    const data = JSON.parse(this.responseText);
+    console.log(data);
+  });
+}
+
+req(1);
+req("");
+req(3);
+
+console.log("end");
