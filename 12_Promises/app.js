057f8bcedf1b158102f660d27eaea8bcf7e8a93a
widenhrzns <widenhrzns@gmail.com>
2025-02-03 23:41:20 +0300

    Обработка reject

--- 12_Promises/app.js
+++ 12_Promises/app.js
@@ -1,10 +1,32 @@
 "use strict";
 
-fetch("https://dummyjson.com/products")
-  .then((response) => response.json())
+// fetch("https://dummyjson.com/productss")
+//   .then(
+//     (response) => {
+//       console.log(response);
+//       return response.json();
+//     },
+//     (error) => console.log(error)
+//   )
+//   .then(
+//     ({ products }) => {
+//       console.log(products);
+//       return fetch("https://dummyjson.com/products/" + products[0].id);
+//     },
+//     (error) => console.log(error)
+//   )
+//   .then((response) => response.json())
+//   .then((data) => console.log(data));
+
+fetch("https://dummyjson.com/productss")
+  .then((response) => {
+    console.log(response);
+    return response.json();
+  })
   .then(({ products }) => {
     console.log(products);
     return fetch("https://dummyjson.com/products/" + products[0].id);
   })
   .then((response) => response.json())
-  .then((data) => console.log(data));
+  .then((data) => console.log(data))
+  .catch((error) => console.log(error));
