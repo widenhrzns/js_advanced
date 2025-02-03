5cdc01e09b95be6c4fddac5d54137983d66d92f5
widenhrzns <widenhrzns@gmail.com>
2025-02-04 00:38:21 +0300

    Упражнение - Select выбора категорий

--- 12_Promises/app.js
+++ 12_Promises/app.js
@@ -1,15 +1,27 @@
 "use strict";
 
-fetch("https://dummyjson.com/productss")
-  .then((response) => {
-    console.log(response);
-    return response.json();
-  })
-  .then(({ products }) => {
-    console.log(products);
-    return fetch("https://dummyjson.com/products/" + products[0].id);
-  })
-  .then((response) => response.json())
-  .then((data) => console.log(data))
-  .catch((error) => console.log(error))
-  .finally(() => console.log("Finally"));
+/*
+    Сделать запрос на https://dummyjson.com/products/categories
+    получить список категорий и отобразить <select> выбора категорий
+*/
+
+function createSelect(array) {
+  const el = document.querySelector(".filter");
+  el.innerHTML = `<select name="" id="">
+        ${array
+          .map((arrEl) => `<option value=${arrEl.slug}>${arrEl.name}</option>`)
+          .join(" ")}
+      </select>
+`;
+}
+
+function getCategories() {
+  fetch("https://dummyjson.com/products/categories")
+    .then((response) => response.json())
+    .then((data) => createSelect(data))
+    .catch((error) => console.error(`Ошибка: ${error}`));
+}
+
+(() => {
+  getCategories();
+})();
