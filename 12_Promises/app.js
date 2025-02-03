26c7320f2ae74e33cfbf166c23b4ac7c3aa98751
widenhrzns <widenhrzns@gmail.com>
2025-02-03 23:45:26 +0300

    Finally

--- 12_Promises/app.js
+++ 12_Promises/app.js
@@ -1,23 +1,5 @@
 "use strict";
 
-// fetch("https://dummyjson.com/productss")
-//   .then(
-//     (response) => {
-//       console.log(response);
-//       return response.json();
-//     },
-//     (error) => console.log(error)
-//   )
-//   .then(
-//     ({ products }) => {
-//       console.log(products);
-//       return fetch("https://dummyjson.com/products/" + products[0].id);
-//     },
-//     (error) => console.log(error)
-//   )
-//   .then((response) => response.json())
-//   .then((data) => console.log(data));
-
 fetch("https://dummyjson.com/productss")
   .then((response) => {
     console.log(response);
@@ -29,4 +11,5 @@ fetch("https://dummyjson.com/productss")
   })
   .then((response) => response.json())
   .then((data) => console.log(data))
-  .catch((error) => console.log(error));
+  .catch((error) => console.log(error))
+  .finally(() => console.log("Finally"));
