530f9f352e4f17c73c7de53921f3217495dd905d
widenhrzns <widenhrzns@gmail.com>
2025-02-11 23:13:36 +0300

    Try catch

--- 14_Modern-Async-JS/app.js
+++ 14_Modern-Async-JS/app.js
@@ -1,18 +1,32 @@
 "use strict";
 
 async function getProducts() {
-  //   fetch("https://dummyjson.com/products")
-  //     .then((response) => response.json())
-  //     .then((data) => console.log(data));
-  const productsResponse = await fetch("https://dummyjson.com/products");
-  const { products } = await productsResponse.json();
-  console.log(products);
+  try {
+    const productsResponse = await fetch("https://dummyjson.com/productss");
+    if (!productsResponse.ok) {
+      throw new Error(productsResponse.status);
+    }
+    const { products } = await productsResponse.json();
+    console.log(products);
+    // throw new Error("Ошибка");
 
-  const productResponse = await fetch(
-    "https://dummyjson.com/products/" + products[0].id
-  );
-  const product = await productResponse.json();
-  console.log(product);
+    const productResponse = await fetch(
+      "https://dummyjson.com/products/" + products[0].id
+    );
+    const product = await productResponse.json();
+    console.log(product);
+  } catch (error) {
+    console.warn(error);
+  } finally {
+    console.log("Finally");
+  }
+}
+
+try {
+  const a = 5;
+  a = 4;
+} catch (error) {
+  console.error(error);
 }
 
 getProducts();
