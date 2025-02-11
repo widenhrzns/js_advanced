0b21f96f70d1ae97bb456bfabfcf957845cc83d2
widenhrzns <widenhrzns@gmail.com>
2025-02-11 15:09:32 +0300

    Пример работы Event Loop

---
+++ 13_Event-Loop/app.js
@@ -0,0 +1,16 @@
+"use strict";
+
+console.log(1);
+
+setTimeout(() => {
+  console.log(2);
+}, 0);
+
+Promise.resolve(3).then((res) => {
+  console.log(res);
+  for (let i = 0; i < 10000000000; i++) {}
+});
+
+console.log(4);
+
+for (let i = 0; i < 10000000000; i++) {}
