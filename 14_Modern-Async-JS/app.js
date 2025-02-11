59c145af3ddf03ef1f6c7388495b788f1f18ae72
widenhrzns <widenhrzns@gmail.com>
2025-02-11 23:00:54 +0300

    Async await

---
+++ 14_Modern-Async-JS/app.js
@@ -0,0 +1,19 @@
+"use strict";
+
+async function getProducts() {
+  //   fetch("https://dummyjson.com/products")
+  //     .then((response) => response.json())
+  //     .then((data) => console.log(data));
+  const productsResponse = await fetch("https://dummyjson.com/products");
+  const { products } = await productsResponse.json();
+  console.log(products);
+
+  const productResponse = await fetch(
+    "https://dummyjson.com/products/" + products[0].id
+  );
+  const product = await productResponse.json();
+  console.log(product);
+}
+
+getProducts();
+console.log("End");
