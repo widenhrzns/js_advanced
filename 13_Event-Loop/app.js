233f8a190bfdde87509cd76a85d4da7aa3363bf0
widenhrzns <widenhrzns@gmail.com>
2025-02-11 16:55:24 +0300

    Статические методы Promise

--- 13_Event-Loop/app.js
+++ 13_Event-Loop/app.js
@@ -1,25 +1,14 @@
 "use strict";
 
-const prom = new Promise((resolve, reject) => {
-  if (new Date() < new Date("02/12/2025")) {
-    reject(new Error("Error"));
-  }
-  resolve("Success");
+Promise.resolve("Instant").then((data) => console.log(data));
+
+const prom = new Promise((resolve) => {
+  console.log("Constructor");
+  for (let i = 0; i < 10000000000; i++) {}
+  setTimeout(() => {
+    resolve("Timer");
+  }, 1000);
 });
+prom.then((data) => console.log(data));
 
-prom.then((data) => console.log(data)).catch((error) => console.log(error));
-
-function timeout(sec) {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve();
-    }, sec * 1000);
-  });
-}
-
-timeout(1)
-  .then(() => {
-    console.log(1);
-    return timeout(1);
-  })
-  .then(() => console.log(2));
+Promise.reject(new Error("Error")).catch((error) => console.error(error));
