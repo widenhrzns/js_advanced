e935773c273f67c136351c93e52a5cb77aa7bb7a
widenhrzns <widenhrzns@gmail.com>
2025-02-14 03:22:04 +0300

    Упражнение - Генератор активностей

--- 14_Modern-Async-JS/app.js
+++ 14_Modern-Async-JS/app.js
@@ -1,19 +1,31 @@
 "use strict";
 
-async function main() {
-  const res = await fetch("https://dummyjson.com/auth/login", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      // 'Authorization': 'Bearer /* YOUR_ACCESS_TOKEN_HERE */',
-    },
-    body: JSON.stringify({
-      username: "emilys",
-      password: "emilyspass",
-    }),
-  });
-  const data = await res.json();
-  console.log(data);
+/*
+  Сделать генератор 3х идей от скуки
+  https://bored.api.lewagon.com/api/activity
+  с отображением на странице
+*/
+const cards = document.querySelector(".cards");
+
+async function getActivity() {
+  const response = await fetch("https://bored.api.lewagon.com/api/activity");
+  return response.json();
 }
 
-main();
+async function generateIdeas() {
+  try {
+    cards.innerHTML = "";
+    const data = await Promise.all([
+      getActivity(),
+      getActivity(),
+      getActivity(),
+    ]);
+    data.forEach((item) => {
+      const card = document.createElement("div");
+      card.innerHTML = item.activity;
+      cards.appendChild(card);
+    });
+  } catch (error) {
+    console.warn(error);
+  }
+}
