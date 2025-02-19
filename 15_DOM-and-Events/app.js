465f64e783e2f381cb8ae7cfd4166bfe76c4c8e7
widenhrzns <widenhrzns@gmail.com>
2025-02-19 23:32:57 +0300

    Пример всплытия событий

--- 15_DOM-and-Events/app.js
+++ 15_DOM-and-Events/app.js
@@ -1,13 +1,39 @@
 "use strict";
 
 const button = document.querySelector(".button");
+const inner = document.querySelector(".inner");
+const wrapper = document.querySelector(".wrapper");
 
-const eventHandler = function (event) {
-  console.log("event1");
-};
+button.addEventListener("click", function (event) {
+  console.log("button");
+  console.log(event.target);
+  console.log(event.currentTarget);
+  this.style.backgroundColor = "#52ebff";
+});
 
-button.addEventListener("mouseover", eventHandler);
-button.addEventListener("click", (event) => {
-  console.log("event2");
-  button.removeEventListener("click", eventHandler);
+inner.addEventListener("click", function (event) {
+  console.log("inner");
+  console.log(event.target);
+  console.log(event.currentTarget);
+  this.style.backgroundColor = "#2a7983";
+
+  //   event.stopPropagation();
+});
+
+wrapper.addEventListener(
+  "click",
+  function (event) {
+    console.log("wrapper");
+    console.log(event.target);
+    console.log(event.currentTarget);
+    this.style.backgroundColor = "#164045";
+  },
+  true
+);
+
+wrapper.addEventListener("click", function (event) {
+  console.log("wrapper");
+  console.log(event.target);
+  console.log(event.currentTarget);
+  this.style.backgroundColor = "#164045";
 });
