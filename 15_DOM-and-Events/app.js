2312a1a1bd3eb6bab9bc9b7b817190bb871e57e2
widenhrzns <widenhrzns@gmail.com>
2025-02-19 23:53:50 +0300

    Делегирование событий

--- 15_DOM-and-Events/app.js
+++ 15_DOM-and-Events/app.js
@@ -1,39 +1,18 @@
 "use strict";
 
-const button = document.querySelector(".button");
-const inner = document.querySelector(".inner");
 const wrapper = document.querySelector(".wrapper");
 
-button.addEventListener("click", function (event) {
-  console.log("button");
-  console.log(event.target);
-  console.log(event.currentTarget);
-  this.style.backgroundColor = "#52ebff";
-});
+for (let i = 0; i < 100; i++) {
+  const el = document.createElement("div");
+  el.innerHTML = `User id: ${i}`;
+  el.setAttribute("data-id", i);
+  //   el.addEventListener("click", () => {
+  //     console.log(`Deleted user ${i}`);
+  //   });
+  wrapper.append(el);
+}
 
-inner.addEventListener("click", function (event) {
-  console.log("inner");
-  console.log(event.target);
-  console.log(event.currentTarget);
-  this.style.backgroundColor = "#2a7983";
-
-  //   event.stopPropagation();
-});
-
-wrapper.addEventListener(
-  "click",
-  function (event) {
-    console.log("wrapper");
-    console.log(event.target);
-    console.log(event.currentTarget);
-    this.style.backgroundColor = "#164045";
-  },
-  true
-);
-
-wrapper.addEventListener("click", function (event) {
-  console.log("wrapper");
-  console.log(event.target);
-  console.log(event.currentTarget);
-  this.style.backgroundColor = "#164045";
+wrapper.addEventListener("click", (event) => {
+  const i = event.target.getAttribute("data-id");
+  console.log(`Deleted user ${i}`);
 });
