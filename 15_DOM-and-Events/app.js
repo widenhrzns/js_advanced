2341e7eff0afb68db8139af6dbf276600807a79a
widenhrzns <widenhrzns@gmail.com>
2025-02-20 01:49:38 +0300

    Упражнение - Поиск по списку

--- 15_DOM-and-Events/app.js
+++ 15_DOM-and-Events/app.js
@@ -1,19 +1,31 @@
 "use strict";
 
-// когда загрзился контент документа
-document.addEventListener("DOMContentLoaded", function (event) {
-  console.log("DOMContentLoaded");
-  console.log(event);
-});
+/*
+    Динамически создать N элементов с текстом и поле
+    для поиска. При вводе в поле, веделять элементы, которые 
+    содержат введенный текст.
+*/
 
-// загрузилось все
-window.addEventListener("load", function (event) {
-  console.log("load");
-  console.log(event);
-});
+const text = document.querySelector(".text");
+const search = document.querySelector(".search");
 
-// перед тем как страница выгружается (например, при переходе на другую страницу спрашивает уверен ли есть не сохраненные данные)
-window.addEventListener("beforeunload", function (event) {
-  event.preventDefault();
-  event.returnValue = "";
+for (let i = 0; i < 100; i++) {
+  const el = document.createElement("div");
+  el.innerText = i;
+  text.append(el);
+}
+
+function pickText(event) {
+  const inputValue = event.target.value;
+  for (const el of [...text.children]) {
+    if (el.innerText.includes(inputValue)) {
+      el.classList.add("picked");
+      continue;
+    }
+    el.classList.remove("picked");
+  }
+}
+
+search.addEventListener("change", (event) => {
+  pickText(event);
 });
