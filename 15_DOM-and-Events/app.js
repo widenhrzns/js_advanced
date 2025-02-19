5c9f6678d5f78eac79e5386d7d92f9e9d64f3390
widenhrzns <widenhrzns@gmail.com>
2025-02-19 22:14:05 +0300

    Визуальное положение элементов

--- 15_DOM-and-Events/app.js
+++ 15_DOM-and-Events/app.js
@@ -1,32 +1,20 @@
 "use strict";
 
-console.log(document.documentElement);
-console.log(document.head);
-console.log(document.body);
+function generate(event) {
+  console.log(event.target.getBoundingClientRect());
 
-const el = document.querySelector(".cards");
-const el2 = document.querySelectorAll("meta");
-console.log(el);
-console.log(el2);
+  console.log(`X OffSet: ${window.pageXOffset}`);
+  console.log(`Y OffSet: ${window.pageYOffset}`);
+  console.log(`clientWidth: ${document.documentElement.clientWidth}`);
+  console.log(`clientHeight: ${document.documentElement.clientHeight}`);
 
-const el3 = document.getElementById("cards");
-const el4 = document.getElementsByClassName("cards");
-console.log(el3);
-console.log(el4);
+  const el = document.querySelector(".down");
+  const rect = el.getBoundingClientRect();
+  console.log(el.getBoundingClientRect());
 
-const el5 = document.getElementsByTagName("meta");
-console.log(el5);
-
-const button = document.createElement("button");
-button.innerHTML = "test";
-const button2 = document.createElement("button");
-button2.innerHTML = "test2";
-
-el.append(button);
-// el.prepend(button2);
-// el.before(button2);
-// el.after(button2);
-
-function generateIdeas() {
-  el.remove();
+  window.scrollTo({
+    left: window.pageXOffset + rect.left,
+    top: window.pageXOffset + rect.top,
+    behavior: "smooth",
+  });
 }
